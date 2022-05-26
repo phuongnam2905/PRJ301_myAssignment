b83799db7f8424c369c92acb7244295464270398
phuongnam2905 <tpn295@gmail.com>
2022-05-26 09:24:36 +0700

    t3 24

---
+++ src/java/FirstServlet.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import static java.lang.System.out;
+
+/**
+ *
+ * @author ADMIN
+ */
+@WebServlet(urlPatterns = {"/First"})
+public class FirstServlet extends HttpServlet {
+
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Integer first = Integer.parseInt(request.getParameter("first"));
+        Integer second = Integer.parseInt(request.getParameter("second"));
+        String operation = request.getParameter("operation");
+        Integer s = 0;
+        PrintWriter out = response.getWriter();
+        if(operation.equals("+")){
+            s = first + second;
+        }
+        else if(operation.equals("-")){
+            s = first - second;
+        }
+        else if(operation.equals("*")){
+            s = first * second;
+        }
+        else if(operation.equals("/")){
+            s  = first / second;
+        }
+        out.println("ket qua" + s);  
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
