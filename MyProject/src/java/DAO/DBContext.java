ad5b5213790fbbfd21642eb0e09b6c7b3636521a
phuongnam2905 <105666943+phuongnam2905@users.noreply.github.com>
2022-06-09 00:25:56 +0700

    .
    .

--- MyProject/src/java/DAO/DBContext.java
+++ MyProject/src/java/DAO/DBContext.java
@@ -22,7 +22,7 @@ public abstract class DBContext<T> {
         try {
             String user = "sa";
             String pass = "123456";
-            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=SE1613";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=rect";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
