4e556f61692b4115650a0a201ac94dc2e54f4a64
phuongnam2905 <105666943+phuongnam2905@users.noreply.github.com>
2022-06-12 23:33:35 +0700

    .
    .

---
+++ MyProject/src/java/DAO/DAOAccount.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Model.Account;
+/**
+ *
+ * @author win
+ */
+public class DAOAccount  extends DBContext<Account> {
+    public Account login(String user, String pass){
+        String sql="select * from Account\n" +
+                   "where [user] =?\n" +
+                   "and pass = ?";
+        try {
+           PreparedStatement stm = connection.prepareStatement(sql);
+              stm.setString(1, user);
+            stm.setString(2, pass);
+            ResultSet rs = stm.executeQuery();
+            
+          while(rs.next()){
+                  return new Account( rs.getString(1), 
+                          rs.getString(2));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+}
---
+++ MyProject/src/java/Model/Account.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author win
+ */
+public class Account {
+     private String user;
+    private String pass;
+    
+    public Account() {
+    }
+
+    public Account(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+
+    }
+
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+}
